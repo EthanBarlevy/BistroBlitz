393bd502ebbe429d27bae6c812d0d7013e2c2a8a
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2023-10-04 15:39:52 -0600

    for some reason the prints arent really working but it shouldnt matter much

--- Source/BistroBlitz/BasicMatchmakingUI.cpp
+++ Source/BistroBlitz/BasicMatchmakingUI.cpp
@@ -20,7 +20,7 @@ void UBasicMatchmakingUI::NativeConstruct()
 CCallResult <UBasicMatchmakingUI, LobbyMatchList_t> m_CallResultLobbyMatchList;
 void UBasicMatchmakingUI::SearchOnClicked()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("ButtonClicked"));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("ButtonClicked"));
 	SteamAPICall_t hSteamAPICall = SteamMatchmaking()->RequestLobbyList();
 	m_CallResultLobbyMatchList.Set(hSteamAPICall, this, &UBasicMatchmakingUI::OnLobbyMatchList);
 }
@@ -28,15 +28,20 @@ void UBasicMatchmakingUI::SearchOnClicked()
 void UBasicMatchmakingUI::OnLobbyMatchList(LobbyMatchList_t *pLobbyMatchList, bool bIOFailure)
 {
 	auto numLobbies = pLobbyMatchList->m_nLobbiesMatching;
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, FString::Printf(TEXT("%f"), numLobbies));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("lobbies: %f"), numLobbies));
 	if (pLobbyMatchList->m_nLobbiesMatching <= 0)
 	{
 		SteamAPICall_t hSteamAPICall = SteamMatchmaking()->CreateLobby(k_ELobbyTypePublic, 2);
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Blue, TEXT("LobbyCreated"));
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("LobbyCreated"));
 	}
 	else
 	{
-		//SteamAPICall_t hSteamAPICall = SteamMatchmaking()->JoinLobby();
+		for (uint64 i = 0; i < numLobbies; i++)
+		{
+			CSteamID LobbyID = SteamMatchmaking()->GetLobbyByIndex(i);
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Purple, FString::Printf(TEXT("lobbyIndex: %f"), 500));
+			//SteamAPICall_t hSteamAPICall = SteamMatchmaking()->JoinLobby(LobbyID);
+		}
 	}
 }
 
